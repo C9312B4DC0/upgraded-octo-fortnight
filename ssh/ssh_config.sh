6fddfb34372239bd646e26b5ca416a9abdbcc65c
C9312B4DC0 <50328+C9312B4DC0@users.noreply.github.com>
2025-09-20 14:13:51 -0400

    Add system messages and logic

--- ssh/ssh_config.sh
+++ ssh/ssh_config.sh
@@ -12,30 +12,35 @@ echo "Hello world!"
 #---> SOME EXPLAINER STUFF
 # Note: I used some Claude Opus 4 to explain some of my options and what they do and to help better understand how to best lay out the script.
 
+#######################################
+# Functions ###########################
+#######################################
 
 #---> Determine package manager for install
-# function set_packagemanager ()
-
 function set_packagemanager() {
     if command -v apt &> /dev/null; then
+		echo "Package manager is apt."
         pkg_install() { sudo apt install -y "$@"; }
         pkg_update()  { sudo apt update -y; }
         pkg_remove()  { sudo apt remove -y "$@"; }
         pkg_search()  { apt search "$@"; }
 
     elif command -v dnf &> /dev/null; then
+		echo "Package manager is dnf."
         pkg_install() { sudo dnf install -y "$@"; }
         pkg_update()  { sudo dnf update -y; }
         pkg_remove()  { sudo dnf remove -y "$@"; }
         pkg_search()  { dnf search "$@"; }
 
 	elif command -v yum &> /dev/null; then
+		echo "Package manager is yum."
         pkg_install() { sudo yum -y install "$@"; }
         pkg_update()  { sudo yum -y update; }
         pkg_remove()  { sudo yum -y remove "$@"; }
         pkg_search()  { yum info "$@"; }
 
     elif command -v pacman &> /dev/null; then
+		echo "Package manager is pacman."
         pkg_install() { sudo pacman -S --noconfirm "$@"; }
         pkg_update()  { sudo pacman -Sy; }
         pkg_remove()  { sudo pacman -R --noconfirm "$@"; }
@@ -47,5 +52,13 @@ function set_packagemanager() {
 	fi
 }
 
-set_packagemanager
-pkg_update
+
+#######################################
+# Main Block ##########################
+#######################################
+echo "Determining package manager for OS..."
+set_packagemanager #---> Determine and set package manager
+
+echo "Updating system packages before installing apps..."
+pkg_update #---> Update system
+
